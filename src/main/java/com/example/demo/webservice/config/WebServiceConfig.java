74fafedaf82498672272df94e20ff977d89ad6dd
Taro Yamada <x8_s8_on4_qge@outlook.jp>
2025-04-10 18:50:09 +0900

    add product end point

--- src/main/java/com/example/demo/webservice/config/WebServiceConfig.java
+++ src/main/java/com/example/demo/webservice/config/WebServiceConfig.java
@@ -1,4 +1,4 @@
-package com.example.demo.webservice;
+package com.example.demo.webservice.config;
 
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.ApplicationContext;
@@ -16,26 +16,49 @@ import org.springframework.xml.xsd.XsdSchema;
 @Configuration
 public class WebServiceConfig extends WsConfigurerAdapter {
 
-    @Bean
-    ServletRegistrationBean<MessageDispatcherServlet> messageDispatcherServlet(ApplicationContext applicationContext) {
+	@Bean
+	ServletRegistrationBean<MessageDispatcherServlet> registHelloWorldServlet(ApplicationContext applicationContext) {
 		MessageDispatcherServlet servlet = new MessageDispatcherServlet();
 		servlet.setApplicationContext(applicationContext);
 		servlet.setTransformWsdlLocations(true);
 		return new ServletRegistrationBean<>(servlet, "/helloWorld/ws/*");
 	}
 
+	@Bean
+	ServletRegistrationBean<MessageDispatcherServlet> registProductServlet(ApplicationContext applicationContext) {
+		MessageDispatcherServlet servlet = new MessageDispatcherServlet();
+		servlet.setApplicationContext(applicationContext);
+		servlet.setTransformWsdlLocations(true);
+		return new ServletRegistrationBean<>(servlet, "/product/ws/*");
+	}
+
 	@Bean(name = "HelloWorld")
-	DefaultWsdl11Definition defaultWsdl11Definition(XsdSchema helloWorldSchema) {
+	DefaultWsdl11Definition helloWorldWsdl11Definition(XsdSchema helloWorldSchema) {
 		DefaultWsdl11Definition wsdl11Definition = new DefaultWsdl11Definition();
 		wsdl11Definition.setPortTypeName("HelloWorldPort");
 		wsdl11Definition.setLocationUri("/helloWorld/ws");
-		wsdl11Definition.setTargetNamespace("http://spring.io/guides/gs-producing-web-service");
+		wsdl11Definition.setTargetNamespace("https://demo-latest-owxa.onrender.com");
 		wsdl11Definition.setSchema(helloWorldSchema);
 		return wsdl11Definition;
 	}
 
+	@Bean(name = "Product")
+	DefaultWsdl11Definition productWsdl11Definition(XsdSchema productSchema) {
+		DefaultWsdl11Definition wsdl11Definition = new DefaultWsdl11Definition();
+		wsdl11Definition.setPortTypeName("ProductPort");
+		wsdl11Definition.setLocationUri("/product/ws");
+		wsdl11Definition.setTargetNamespace("https://demo-latest-owxa.onrender.com");
+		wsdl11Definition.setSchema(productSchema);
+		return wsdl11Definition;
+	}
+
 	@Bean
-	XsdSchema countriesSchema() {
+	XsdSchema helloWorldSchema() {
 		return new SimpleXsdSchema(new ClassPathResource("helloWorld.xsd"));
+	}
+
+	@Bean
+	XsdSchema productSchema() {
+		return new SimpleXsdSchema(new ClassPathResource("product.xsd"));
 	}
 }
--- src/main/java/com/example/demo/webservice/endpoint/HelloWorldEndPoint.java
+++ src/main/java/com/example/demo/webservice/endpoint/HelloWorldEndPoint.java
@@ -1,13 +1,17 @@
-package com.example.demo.webservice;
+package com.example.demo.webservice.endpoint;
 
 import org.springframework.ws.server.endpoint.annotation.Endpoint;
 import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
 import org.springframework.ws.server.endpoint.annotation.RequestPayload;
 import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
 
+import com.example.demo.webservice.GetHelloWorldRequest;
+import com.example.demo.webservice.GetHelloWorldResponse;
+
 @Endpoint
 public class HelloWorldEndPoint {
-	private static final String NAMESPACE_URI = "http://spring.io/guides/gs-producing-web-service";
+
+	private static final String NAMESPACE_URI = "https://demo-latest-owxa.onrender.com";
 
 	@PayloadRoot(namespace = NAMESPACE_URI, localPart = "getHelloWorldRequest")
 	@ResponsePayload
